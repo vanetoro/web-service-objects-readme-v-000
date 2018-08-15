72714056c12e73a74c3cfaf77fabfc8fc54e5266
Vanessa Toro <vanetoro84@gmail.com>
2018-08-15 17:11:04 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -3,13 +3,13 @@ class FoursquareService
 
   def authenticate!(client_id, client_secret, code)
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
-      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
-      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
       req.params['grant_type'] = 'authorization_code'
       req.params['redirect_uri'] = "http://localhost:3000/auth"
-      req.params['code'] = params[:code]
+      req.params['code'] = code
     end
     body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
-    redirect_to root_path
-end 
+
+end
