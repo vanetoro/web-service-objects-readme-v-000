88a287f85acac827d8450390ad7fe9bd32829577
Vanessa Toro <vanetoro84@gmail.com>
2018-08-15 17:18:11 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -6,7 +6,7 @@ class FoursquareService
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['redirect_uri'] = "http://67.205.182.198:55372/auth"
       req.params['code'] = code
     end
     body = JSON.parse(resp.body)
